6fcd2b24de817a208b9253d99c2d03f783b158d8
justusmattern27 <justus.mattern@rwth-aachen.de>
2024-03-19 17:27:59 +0100

    feat: new example project for create package

--- packages/create/template-2d-js/src/project.js
+++
@@ -1,7 +0,0 @@
-import {makeProject} from '@revideo/core';
-
-import example from './scenes/example?scene';
-
-export default makeProject({
-  scenes: [example],
-});
--- packages/create/template-2d-ts/src/scenes/example.tsx
+++ packages/create/template-2d-ts/src/scenes/example.tsx
@@ -1,12 +1,58 @@
-import {Circle, makeScene2D} from '@revideo/2d';
-import {createRef} from '@revideo/core';
+import {Audio, Img, Layout, Rect, Txt, Video, makeScene2D} from '@revideo/2d';
+import {all, chain, createRef, waitFor} from '@revideo/core';
 
 export default makeScene2D(function* (view) {
-  // Create your animations here
+  const upperBox = createRef<Rect>();
+  const logoRef = createRef<Img>();
 
-  const circle = createRef<Circle>();
+  view.add(
+    <>
+      <Layout direction={'column'} size={[1024, 1792]} layout={true}>
+        <Rect
+          fill={'#FFFFFF'}
+          size={['100%', '40%']}
+          direction={'column'}
+          gap={'20%'}
+          ref={upperBox}
+          justifyContent={'center'}
+          alignItems={'center'}
+          layout={true}
+        />
+        <Video
+          src={
+            'https://revideo-example-assets.s3.amazonaws.com/aurora-borealis.mp4'
+          }
+          size={['100%', '60%']}
+          play={true}
+        />
+      </Layout>
+      <Audio
+        src={
+          'https://revideo-example-assets.s3.amazonaws.com/heart-on-my-sleeve.mp3'
+        }
+        play={true}
+        time={6.6}
+      />
+    </>,
+  );
 
-  view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
+  yield* waitFor(0.5);
 
-  yield* circle().scale(2, 2).to(1, 2);
+  upperBox().add(
+    <Txt fontWeight={700} fontSize={70}>
+      Hello! Welcome to
+    </Txt>,
+  );
+  upperBox().add(
+    <Img
+      width={'1%'}
+      ref={logoRef}
+      src={'https://revideo-example-assets.s3.amazonaws.com/revideo-banner.png'}
+    />,
+  );
+
+  yield* chain(
+    all(logoRef().scale(30, 2), logoRef().rotation(360, 2)),
+    logoRef().scale(40, 2),
+  );
 });

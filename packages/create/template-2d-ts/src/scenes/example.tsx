c89a9a441caf018b0863e62be8c4a63754085e3f
justusmattern27 <justus.mattern@rwth-aachen.de>
2024-03-20 17:34:49 +0100

    feat: simpler example project

--- packages/create/template-2d-ts/src/scenes/example.tsx
+++ packages/create/template-2d-ts/src/scenes/example.tsx
@@ -1,4 +1,4 @@
-import {Audio, Img, Layout, Rect, Txt, Video, makeScene2D} from '@revideo/2d';
+import {Audio, Img, Rect, Video, makeScene2D} from '@revideo/2d';
 import {all, chain, createRef, waitFor} from '@revideo/core';
 
 export default makeScene2D(function* (view) {
@@ -7,52 +7,33 @@ export default makeScene2D(function* (view) {
 
   view.add(
     <>
-      <Layout direction={'column'} size={[1024, 1792]} layout={true}>
-        <Rect
-          fill={'#FFFFFF'}
-          size={['100%', '40%']}
-          direction={'column'}
-          gap={'20%'}
-          ref={upperBox}
-          justifyContent={'center'}
-          alignItems={'center'}
-          layout={true}
-        />
-        <Video
-          src={
-            'https://revideo-example-assets.s3.amazonaws.com/aurora-borealis.mp4'
-          }
-          size={['100%', '60%']}
-          play={true}
-        />
-      </Layout>
-      <Audio
-        src={
-          'https://revideo-example-assets.s3.amazonaws.com/heart-on-my-sleeve.mp3'
-        }
+      <Video
+        src={'https://revideo-example-assets.s3.amazonaws.com/stars.mp4'}
+        size={['100%', '100%']}
         play={true}
-        time={6.6}
+      />
+      <Audio
+        src={'https://revideo-example-assets.s3.amazonaws.com/chill-beat.mp3'}
+        play={true}
+        time={17.0}
       />
     </>,
   );
 
-  yield* waitFor(0.5);
+  yield* waitFor(1);
 
-  upperBox().add(
-    <Txt fontWeight={700} fontSize={70}>
-      Hello! Welcome to
-    </Txt>,
-  );
-  upperBox().add(
+  view.add(
     <Img
       width={'1%'}
       ref={logoRef}
-      src={'https://revideo-example-assets.s3.amazonaws.com/revideo-banner.png'}
+      src={
+        'https://revideo-example-assets.s3.amazonaws.com/revideo-logo-white.png'
+      }
     />,
   );
 
   yield* chain(
-    all(logoRef().scale(30, 2), logoRef().rotation(360, 2)),
-    logoRef().scale(40, 2),
+    all(logoRef().scale(40, 2), logoRef().rotation(360, 2)),
+    logoRef().scale(60, 1),
   );
 });

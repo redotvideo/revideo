632d8c61feda05aed5e6e21d02a8e1add4bc5dd8
hkonsti <45428767+hkonsti@users.noreply.github.com>
2024-04-26 11:13:01 +0200

    refactor: pt 2

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
-import {execSync} from 'child_process';
 import {Command} from 'commander';
-import path from 'path';
+import {app} from './server/index';
 
-const Program = new Command();
+const program = new Command();
 
-Program.name('revideo')
+program
+  .name('revideo')
   .description('CLI to interact with the revideo service')
-  .version('1.0.0');
+  .version('0.2.13');
 
-Program.command('serve')
+program
+  .command('serve')
   .description('Start the revideo server')
   .option('--projectFile <path>', 'Path to the project file')
   .option('--port <number>', 'Port on which to start the server', '4000')
@@ -22,7 +23,10 @@ Program.command('serve')
     const {projectFile, port} = options;
     process.env.PROJECT_FILE = projectFile;
     process.env.REVIDEO_PORT = port;
-    execSync(`node ${path.join(__dirname, './server.js')}`, {stdio: 'inherit'});
+
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
   });
 
-Program.parse(process.argv);
+program.parse(process.argv);

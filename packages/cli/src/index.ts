dd115fba52a9c5e9c6617cf76b1f1428dcb9a57d
hkonsti <45428767+hkonsti@users.noreply.github.com>
2024-04-26 16:06:52 +0200

    feat: add project.js file and reloading

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 import {Command} from 'commander';
-import {app} from './server/index';
+import {createServer} from './server/index';
+import {buildProject} from './server/player';
 
 const program = new Command();
 
@@ -12,10 +13,12 @@ program
 
 program
   .command('serve')
-  .description('Start the revideo server')
+  .description('Start the revideo server.')
   .option('--projectFile <path>', 'Path to the project file')
   .option('--port <number>', 'Port on which to start the server', '4000')
-  .action(options => {
+  .action(async options => {
+    await buildProject();
+
     if (!options.projectFile) {
       console.error('Error: --projectFile option must be specified.');
       process.exit(1);
@@ -24,8 +27,38 @@ program
     process.env.PROJECT_FILE = projectFile;
     process.env.REVIDEO_PORT = port;
 
-    app.listen(port, () => {
+    createServer().listen(port, () => {
       console.log(`Server listening on port ${port}`);
+    });
+  });
+
+program
+  .command('dev')
+  .description(
+    'Start the revideo server in development mode. Watches for changes ' +
+      'in the project directory and re-builds the player on each change.',
+  )
+  .option('--projectFile <path>', 'Path to the project file')
+  .option('--port <number>', 'Port on which to start the server', '4000')
+  .option('--watchDir <path>', 'Directory to watch for changes', 'src')
+  .action(async options => {
+    if (!options.projectFile) {
+      console.error('Error: --projectFile option must be specified.');
+      process.exit(1);
+    }
+
+    await buildProject().catch(() => {
+      console.error('Error building project');
+      process.exit(1);
+    });
+
+    const {projectFile, port} = options;
+    process.env.PROJECT_FILE = projectFile;
+    process.env.REVIDEO_PORT = port;
+
+    createServer(options.watchDir).listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+      console.log();
     });
   });
 
--- packages/cli/src/server/index.ts
+++ packages/cli/src/server/index.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import {download} from './download';
+import {createHotReloader, player} from './player';
 import {render} from './render';
 
-export const app = express();
+export function createServer(hotReloadDir?: string) {
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-app.post('/render', render);
-app.get('/download/:projectName', download);
+  app.post('/render', render);
+  app.get('/download/:projectName', download);
+
+  app.use('/player/project.js', player);
+
+  if (hotReloadDir) {
+    createHotReloader(hotReloadDir);
+  }
+
+  return app;
+}

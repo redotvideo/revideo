09967c727b42f664c6be892c779cf00c26528aa7
hkonsti <45428767+hkonsti@users.noreply.github.com>
2024-05-03 14:16:26 +0200

    feat: merge commands, add telemetry

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import {EventName, sendEvent} from '@revideo/telemetry';
 import {Command} from 'commander';
 import {createServer} from './server/index';
 import {buildProject} from './server/player';
@@ -13,33 +14,8 @@ program
 
 program
   .command('serve')
-  .description('Start the revideo server.')
-  .option(
-    '--projectFile <path>',
-    'Path to the project file',
-    './vite.config.ts',
-  )
-  .option('--port <number>', 'Port on which to start the server', '4000')
-  .action(async options => {
-    await buildProject();
-
-    if (!options.projectFile) {
-      console.error('Error: --projectFile option must be specified.');
-      process.exit(1);
-    }
-    const {projectFile, port} = options;
-    process.env.PROJECT_FILE = projectFile;
-    process.env.REVIDEO_PORT = port;
-
-    createServer().listen(port, () => {
-      console.log(`Server listening on port ${port}`);
-    });
-  });
-
-program
-  .command('dev')
   .description(
-    'Start the revideo server in development mode. Watches for changes ' +
+    'UNSTABLE (still WIP): Start the revideo server in development mode. Watches for changes ' +
       'in the project directory and re-builds the player on each change.',
   )
   .option(
@@ -50,6 +26,8 @@ program
   .option('--port <number>', 'Port on which to start the server', '4000')
   .option('--watchDir <path>', 'Directory to watch for changes', 'src')
   .action(async options => {
+    sendEvent(EventName.CLICommand);
+
     if (!options.projectFile) {
       console.error('Error: --projectFile option must be specified.');
       process.exit(1);

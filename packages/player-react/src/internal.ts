56953c05c271d38e96d5a9ca490140d17af55c47
hkonsti <45428767+hkonsti@users.noreply.github.com>
2024-05-10 19:34:59 +0200

    feat: add player component package

---
+++ packages/player-react/src/internal.ts
@@ -0,0 +1,279 @@
+import type {PlayerSettings, Project, StageSettings} from '@revideo/core';
+import {Player, Stage} from '@revideo/core';
+
+import {Vector2} from '@revideo/core';
+
+const stylesNew = `
+.overlay {
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	opacity: 0;
+	transition: opacity 0.1s;
+	z-index: 0;
+  }
+  .canvas {
+	width: 100%;
+	display: block;
+	opacity: 1;
+	transition: opacity 0.1s;
+  }
+`;
+
+const TEMPLATE = `<style>${stylesNew}</style><div class="overlay"></div>`;
+const ID = 'revideo-player';
+
+enum State {
+  Initial = 'initial',
+  Loading = 'loading',
+  Ready = 'ready',
+  Error = 'error',
+}
+
+class MotionCanvasPlayer extends HTMLElement {
+  public static get observedAttributes() {
+    return [
+      'src',
+      'playing',
+
+      'quality',
+      'width',
+      'height',
+      'auto',
+      'variables',
+    ];
+  }
+
+  private get auto() {
+    const attr = this.getAttribute('auto');
+    return !!attr;
+  }
+
+  private get quality() {
+    const attr = this.getAttribute('quality');
+    return attr ? parseFloat(attr) : this.defaultSettings?.resolutionScale ?? 1;
+  }
+
+  private get width() {
+    const attr = this.getAttribute('width');
+    return attr ? parseFloat(attr) : this.defaultSettings?.size.width ?? 0;
+  }
+
+  private get height() {
+    const attr = this.getAttribute('height');
+    return attr ? parseFloat(attr) : this.defaultSettings?.size.height ?? 0;
+  }
+
+  private get variables() {
+    try {
+      const attr = this.getAttribute('variables');
+      return attr ? JSON.parse(attr) : {};
+    } catch {
+      this.project?.logger.warn(`Project variables could not be parsed.`);
+      return {};
+    }
+  }
+
+  private readonly root: ShadowRoot;
+  private readonly canvas: HTMLCanvasElement;
+  private readonly overlay: HTMLCanvasElement;
+
+  private state = State.Initial;
+  private project: Project | null = null;
+  private player: Player | null = null;
+  private defaultSettings: (PlayerSettings & StageSettings) | undefined;
+  private abortController: AbortController | null = null;
+  private playing = false;
+  private stage = new Stage();
+
+  private time: number = 0;
+  private duration: number = 0; // in frames
+
+  public constructor() {
+    super();
+    this.root = this.attachShadow({mode: 'open'});
+    this.root.innerHTML = TEMPLATE;
+
+    this.overlay = this.root.querySelector('.overlay')!;
+    this.canvas = this.stage.finalBuffer;
+    this.canvas.classList.add('canvas');
+    this.root.prepend(this.canvas);
+
+    this.setState(State.Initial);
+  }
+
+  private setState(state: State) {
+    this.state = state;
+    this.setPlaying(this.playing);
+  }
+
+  private setPlaying(value: boolean) {
+    if (this.state === State.Ready && value) {
+      this.player?.togglePlayback(true);
+      this.playing = true;
+    } else {
+      this.player?.togglePlayback(false);
+      this.playing = false;
+    }
+  }
+
+  private async updateSource(source: string) {
+    this.setState(State.Initial);
+
+    this.abortController?.abort();
+    this.abortController = new AbortController();
+
+    let project: Project;
+    try {
+      const promise = import(
+        /* webpackIgnore: true */ /* @vite-ignore */ source
+      );
+      const delay = new Promise(resolve => setTimeout(resolve, 200));
+      await Promise.any([delay, promise]);
+      this.setState(State.Loading);
+      project = (await promise).default;
+    } catch (e) {
+      console.error(e);
+      this.setState(State.Error);
+      return;
+    }
+
+    this.defaultSettings = project.meta.getFullRenderingSettings();
+    const player = new Player(project, {fps: 30});
+    player.setVariables(this.variables);
+
+    this.player?.onRender.unsubscribe(this.render);
+    this.player?.onFrameChanged.unsubscribe(this.handleFrameChanged);
+    this.player?.togglePlayback(false);
+    this.player?.deactivate();
+    this.project = project;
+    this.player = player;
+
+    // TODO: remove
+    console.log('fps', this.player.playback.fps);
+
+    this.updateSettings();
+    this.player.onRender.subscribe(this.render);
+    this.player.onFrameChanged.subscribe(this.handleFrameChanged);
+    this.player.togglePlayback(this.playing);
+
+    this.setState(State.Ready);
+  }
+
+  public attributeChangedCallback(name: string, _: any, newValue: any) {
+    switch (name) {
+      case 'auto':
+        this.setPlaying(this.playing);
+        break;
+      case 'src':
+        this.updateSource(newValue);
+        break;
+      case 'playing':
+        this.setPlaying(newValue === 'true');
+        break;
+      case 'quality':
+      case 'width':
+      case 'height':
+        this.updateSettings();
+        break;
+      case 'variables':
+        this.player?.setVariables(this.variables);
+        this.player?.requestSeek(this.player.playback.frame); // reseek to update variables immediately
+    }
+  }
+
+  /**
+   * Runs when the element is removed from the DOM.
+   */
+  public disconnectedCallback() {
+    this.player?.deactivate();
+    this.player?.onRender.unsubscribe(this.render);
+
+    this.removeEventListener('seekto', this.handleSeekTo);
+  }
+
+  /**
+   * Runs when the element is added to the DOM.
+   */
+  public connectedCallback() {
+    this.player?.activate();
+    this.player?.onRender.subscribe(this.render);
+
+    this.addEventListener('seekto', this.handleSeekTo);
+  }
+
+  /**
+   * Triggered by the timeline.
+   */
+  private handleSeekTo = (event: Event) => {
+    if (!this.project) {
+      return;
+    }
+
+    const e = event as CustomEvent;
+    this.time = e.detail;
+    this.player?.requestSeek(
+      e.detail * this.project.meta.getFullPreviewSettings().fps,
+    );
+  };
+
+  /**
+   * Triggered by the player.
+   */
+  private handleFrameChanged = (frame: number) => {
+    if (!this.project) {
+      return;
+    }
+    this.time = frame / this.project.meta.getFullPreviewSettings().fps;
+  };
+
+  /**
+   * Called on every frame.
+   */
+  private render = async () => {
+    if (this.player && this.project) {
+      await this.stage.render(
+        this.player.playback.currentScene,
+        this.player.playback.previousScene,
+      );
+
+      this.dispatchEvent(new CustomEvent('timeupdate', {detail: this.time}));
+
+      const durationInFrames = this.player.playback.duration;
+      if (durationInFrames === this.duration) {
+        return;
+      }
+
+      this.duration = durationInFrames;
+
+      const durationInSeconds =
+        durationInFrames / this.project.meta.getFullPreviewSettings().fps;
+      this.dispatchEvent(
+        new CustomEvent('duration', {detail: durationInSeconds}),
+      );
+    }
+  };
+
+  private updateSettings() {
+    if (!this.defaultSettings) {
+      return;
+    }
+
+    const settings = {
+      ...this.defaultSettings,
+      size: new Vector2(this.width, this.height),
+      resolutionScale: this.quality,
+    };
+    this.stage.configure(settings);
+    this.player?.configure(settings);
+  }
+}
+
+if (!customElements.get(ID)) {
+  customElements.define(ID, MotionCanvasPlayer);
+}

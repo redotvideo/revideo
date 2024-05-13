37cc8596807a3ad8ab06b7358145db01ad4d8eb0
hkonsti <45428767+hkonsti@users.noreply.github.com>
2024-05-13 13:49:52 +0200

    chore: get rid of warning

--- packages/create/next/app/page.tsx
+++ packages/create/next/app/page.tsx
@@ -1,6 +1,34 @@
 'use client';
-import {Player} from '../components/player';
+import {Player} from '@revideo/player-react';
+import {useState} from 'react';
 
 export default function Home() {
-  return <Player />;
+  const [playing, setPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  function handleTimeUpdate(currentTime: number) {
+    setCurrentTime(currentTime);
+  }
+
+  function handleDurationChange(duration: number) {
+    setDuration(duration);
+  }
+
+  return (
+    <>
+      {currentTime.toFixed(2)} / {duration.toFixed(2)}
+      <button onClick={() => setPlaying(prev => !prev)}>Play/Pause</button>
+      <div className="flex max-w-6xl">
+        <Player
+          src="/project.js"
+          playing={playing}
+          currentTime={currentTime}
+          onTimeUpdate={handleTimeUpdate}
+          onDurationChange={handleDurationChange}
+          controls={true}
+        />
+      </div>
+    </>
+  );
 }

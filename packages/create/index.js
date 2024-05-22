d2ea8e54821ea55d566f66e32550513272fe037e
hkonsti <45428767+hkonsti@users.noreply.github.com>
2024-05-22 15:48:16 +0200

    chore: simplify index.js and add other examples

--- packages/create/index.js
+++ packages/create/index.js
@@ -7,53 +7,21 @@ import {fileURLToPath} from 'node:url';
 import path from 'path';
 import prompts from 'prompts';
 
-const FILES_TO_MODIFY = {
-  gitignore: '.gitignore',
-  '.gitkeep': false,
-};
+const templates = [
+  'default',
+  'avatar-with-background',
+  'google-cloud-run',
+  'google-cloud-run-parallelized',
+  'stitching-videos',
+  'youtube-shorts',
+];
 
-const MANIFEST = JSON.parse(
-  fs.readFileSync(
-    path.resolve(fileURLToPath(import.meta.url), '../package.json'),
-    'utf-8',
-  ),
-);
-
-const PLUGINS = {
-  core: {
-    package: '@revideo/vite-plugin',
-    variable: 'motionCanvas',
-    options: response =>
-      response.language === 'js' ? `{project: './src/project.js'}` : '',
-  },
-};
-
-(async () => {
+async function run() {
   const options = minimist(process.argv.slice(2));
-  if (options.plugins !== undefined) {
-    if (typeof options.plugins === 'string') {
-      options.plugins = options.plugins.split(',');
-    }
-
-    if (!Array.isArray(options.plugins)) {
-      options.plugins = [options.plugins];
-    }
-
-    const plugins = ['core'];
-    for (const plugin of options.plugins) {
-      if (plugin === 'core') continue;
-      if (!(plugin in PLUGINS)) {
-        console.log(kleur.yellow(`! Unknown plugin "${plugin}".\n`));
-        continue;
-      }
-      plugins.push(plugin);
-    }
-
-    options.plugins = plugins;
-  }
 
   prompts.override(options);
   const response = await prompts([
+    // Prompt for project name
     {
       type: 'text',
       name: 'name',
@@ -64,6 +32,7 @@ const PLUGINS = {
           ? true
           : 'Project name must be a valid npm package name.',
     },
+    // Prompt for project path
     {
       type: 'text',
       name: 'path',
@@ -90,42 +59,46 @@ const PLUGINS = {
       },
       format: value => path.resolve(value),
     },
+    // Prompt for which example to scaffold
+    {
+      type: 'select',
+      name: 'starter',
+      message: 'Choose a starter template',
+
+      choices: [
+        ...templates.map(template => ({title: template, value: template})),
+      ],
+    },
   ]);
 
+  // Abort if the user didn't provide a project name
   if (!response.path) {
     console.log(kleur.red('× Scaffolding aborted by the user.\n'));
     return;
   }
 
-  const plugins = [PLUGINS.core];
+  console.log(`the response is ${JSON.stringify(response)}`);
+
+  // Clone files
   const templateDir = path.resolve(
     fileURLToPath(import.meta.url),
     '..',
-    `examples/default`,
+    `examples/${response.starter}`,
   );
   copyDirectory(templateDir, response.path);
-  createConfig(response, plugins);
+  createConfig(response);
 
+  // Read package.json and modify name
   const manifest = JSON.parse(
     fs.readFileSync(path.join(templateDir, `package.json`), 'utf-8'),
   );
   manifest.name = response.name;
-  manifest.dependencies ??= {};
-  for (const data of plugins) {
-    if (data.version) {
-      manifest.dependencies[data.package] = data.version;
-    }
-  }
-  cloneVersions(manifest.dependencies);
-
-  if (manifest.devDependencies) {
-    cloneVersions(manifest.devDependencies);
-  }
   fs.writeFileSync(
     path.join(response.path, 'package.json'),
     JSON.stringify(manifest, undefined, 2),
   );
 
+  // Tell user that the process is complete
   const manager = getPackageManager();
   console.log(kleur.green('\n√ Scaffolding complete. You can now run:'));
   if (response.path !== process.cwd()) {
@@ -142,7 +115,7 @@ const PLUGINS = {
     console.log(`  ${boldManager} start`);
   }
   console.log();
-})();
+}
 
 function isValidPackageName(projectName) {
   return /^(?:@[a-z0-9-*~][a-z0-9-*._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/.test(
@@ -153,13 +126,8 @@ function isValidPackageName(projectName) {
 function copyDirectory(src, dest) {
   fs.mkdirSync(dest, {recursive: true});
   for (const file of fs.readdirSync(src)) {
-    let target = file;
-    if (file in FILES_TO_MODIFY) {
-      if (FILES_TO_MODIFY[file] === false) continue;
-      target = FILES_TO_MODIFY[file];
-    }
     const srcFile = path.resolve(src, file);
-    const destFile = path.resolve(dest, target);
+    const destFile = path.resolve(dest, file);
     copy(srcFile, destFile);
   }
 }
@@ -173,27 +141,17 @@ function copy(src, dest) {
   }
 }
 
-function createConfig(response, selectedPlugins) {
-  const imports = [];
-  const plugins = [];
-  for (const data of selectedPlugins) {
-    imports.push(`import ${data.variable} from '${data.package}';\n`);
-    plugins.push(`${data.variable}(${data.options?.(response) ?? ''}),`);
-  }
-
+function createConfig(response) {
   const configFile = path.resolve(response.path, `vite.config.ts`);
 
   fs.writeFileSync(
     configFile,
-    `import {defineConfig} from 'vite';
-${imports.join('')}
-
-export default defineConfig({
-  plugins: [
-    ${plugins.join('\n    ')}
-  ],
-});
-`,
+    `import motionCanvas from '@revideo/vite-plugin';
+    import {defineConfig} from 'vite';
+    
+    export default defineConfig({
+      plugins: [motionCanvas()],
+    });`,
   );
 }
 
@@ -202,13 +160,4 @@ function getPackageManager() {
   return ua?.split(' ')[0].split('/')[0] ?? 'npm';
 }
 
-function cloneVersions(versions) {
-  for (const dependency in versions) {
-    if (
-      dependency.startsWith('@revideo') &&
-      MANIFEST.devDependencies[dependency]
-    ) {
-      versions[dependency] = MANIFEST.devDependencies[dependency];
-    }
-  }
-}
+void run();

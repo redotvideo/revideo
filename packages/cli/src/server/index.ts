230ff04a0cc50aff97304bb9e9518fac756ddb53
Konsti <45428767+hkonsti@users.noreply.github.com>
2024-06-02 19:04:30 +0200

    chore: increase cli json body size limit to 50mb (#118)

--- packages/cli/src/server/index.ts
+++ packages/cli/src/server/index.ts
@@ -7,7 +7,7 @@ import {render} from './render';
 export function createServer(hotReloadDir?: string) {
   const app = express();
 
-  app.use(express.json());
+  app.use(express.json({limit: '50mb'}));
   app.use(cors());
 
   app.post('/render', render);

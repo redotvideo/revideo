ed9a26d12eabf2237fc18b2adbd5c048e4033583
justusmattern27 <justus.mattern@rwth-aachen.de>
2024-07-15 11:35:05 +0200

    ci: release v0.4.8

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .name('revideo')
   .description('CLI to interact with the revideo service')
-  .version('0.4.7');
+  .version('0.4.8');
 
 program
   .command('serve')

56900092aa3de403f57d86cfcf8ae72789d75ec5
Justus Mattern <122226645+justusmattern27@users.noreply.github.com>
2024-08-14 14:45:04 +0200

    ci: release v0.5.5 (#247)
    * ci: release v0.5.5
    
    * ci(release): 0.5.5 [skip ci]
    
    ---------
    
    Co-authored-by: revideo-bot <167322027+revideo-bot@users.noreply.github.com>

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .name('revideo')
   .description('CLI to interact with the revideo service')
-  .version('0.5.4');
+  .version('0.5.5');
 
 program
   .command('serve')

7abfd3d6811c6fa7be808ede04a0d7ddf1ae466b
hkonsti <45428767+hkonsti@users.noreply.github.com>
2024-09-17 20:57:35 +0200

    chore: package-lock and clean up bootstrap

--- packages/core/src/app/bootstrap.ts
+++ packages/core/src/app/bootstrap.ts
@@ -1,50 +1,13 @@
 import {Plugin} from '../plugin';
 import DefaultPlugin from '../plugin/DefaultPlugin';
 import {Logger} from './Logger';
-import {
-  BaseProject,
-  createVersionObject,
-  FullProject,
-  UserSettings,
-} from './Project';
+import {BaseProject, createVersionObject, FullProject} from './Project';
 
-/**
- * // TODO(refactor): delete
- * Bootstrap a project.
- *
- * @param name - The name of the project.
- * @param versions - Package versions.
- * @param config - Project settings.
- * @param settingsFile - The settings meta file.
- * @param logger - An optional logger instance.
- *
- * @internal
- */
-export function bootstrap(
-  name: string,
-  config: BaseProject,
-  settings: UserSettings,
-): FullProject {
+export function makeProject(project: BaseProject): FullProject {
   // Don't delete, has side effects
   // TODO(konsti): Figure out how to get rid of this
   void DefaultPlugin;
 
-  const version = '0.5.9';
-
-  const project = {
-    name,
-    ...config,
-    plugins: [] as Plugin[], // TODO(refactor): get rid of this
-    versions: createVersionObject(version),
-    logger: new Logger(),
-    settings,
-  } as FullProject;
-
-  return project;
-}
-
-// TODO(refactor): naming
-export function bootstrapNew(project: BaseProject): FullProject {
   return {
     ...project,
     name: project.name ?? 'project',

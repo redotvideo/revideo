f2afc90616273aa4a7369c4692f4a78bd2776308
hkonsti <45428767+hkonsti@users.noreply.github.com>
2024-09-22 19:19:10 +0200

    feat: new docs

--- packages/docs/src/theme/MDXComponents.js
+++
@@ -1,7 +0,0 @@
-import FiddleCodeBlock from '@site/src/components/Fiddle/FiddleCodeBlock';
-import MDXComponents from '@theme-original/MDXComponents';
-
-export default {
-  ...MDXComponents,
-  pre: FiddleCodeBlock,
-};
--- packages/docs/typedoc-standalone.js
+++
@@ -1,7 +0,0 @@
-const factory = require('./typedoc.js');
-
-(async () => {
-  process.env.NODE_ENV = 'production';
-  const plugin = factory();
-  await plugin.loadContent();
-})();

73b2ffbee93f16f8380f4b13cb36a898a627a369
hkonsti <45428767+hkonsti@users.noreply.github.com>
2024-12-06 14:00:11 +0100

    chore: update versions to 0.10.2

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .name('revideo')
   .description('CLI to interact with the revideo service')
-  .version('0.10.1');
+  .version('0.10.2');
 
 program
   .command('serve')
